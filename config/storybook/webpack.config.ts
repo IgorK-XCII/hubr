2e745fda5667dde908049aec5122b012750e3865
Kapustin I.V <IgorK_XCII@pm.me>
2023-03-12 22:40:35 +0300

    fix config style

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,32 +1,29 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 import { Configuration, RuleSetRule } from 'webpack';
 import { buildCssLoader, buildSvgLoader } from '../build/loaders';
 import { buildDefinePlugin } from '../build/plugins';
 
 export default ({ config }: { config: Configuration }) => {
-  if (config.resolve) {
-    if (!config.resolve.plugins) config.resolve.plugins = [];
+  if (!config.resolve!.plugins) config.resolve!.plugins = [];
 
-    config.resolve.plugins.push(new TsconfigPathsPlugin());
-    config.resolve.extensions?.push('ts', 'tsx');
-    config.resolve.fallback = {
-      http: false,
-    };
-  }
+  config.resolve!.plugins.push(new TsconfigPathsPlugin());
+  config.resolve!.extensions?.push('ts', 'tsx');
+  config.resolve!.fallback = {
+    http: false,
+  };
 
-  if (config.module) {
-    config.module.rules = config.module.rules?.map((rule) => {
-      const _rule = rule as RuleSetRule;
+  config.module!.rules = config.module!.rules?.map((rule) => {
+    const _rule = rule as RuleSetRule;
 
-      if (/svg/.test(_rule.test as string)) {
-        return { ..._rule, exclude: /\.svg$/i };
-      }
+    if (/svg/.test(_rule.test as string)) {
+      return { ..._rule, exclude: /\.svg$/i };
+    }
 
-      return rule;
-    });
+    return rule;
+  });
 
-    config.module.rules?.push(buildSvgLoader(), buildCssLoader(true));
-  }
+  config.module!.rules?.push(buildSvgLoader(), buildCssLoader(true));
 
   config.plugins?.push(buildDefinePlugin(true, ''));
 
